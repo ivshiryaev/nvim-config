5067f4a08f8ebefa2f2ddc5647b92601ecb1a1cf
🐈 Ivan Shyriaiev <ivshiryaev1999@gmail.com>
2026-03-15 02:23:13 +0100

    feat: add autotag plugin so all <html> elements are closed automatically

--- init.lua
+++ init.lua
@@ -852,6 +852,14 @@ require('lazy').setup({
     end,
   },
 
+  {
+    'windwp/nvim-ts-autotag',
+    event = 'BufReadPre',
+    config = function()
+      require('nvim-ts-autotag').setup()
+    end
+  },
+
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
